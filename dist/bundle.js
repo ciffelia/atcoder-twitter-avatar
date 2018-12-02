8db697c0977f3de1c44ba63b838efe5ba332a9e3
prince0203 <mc.prince.0203@gmail.com>
2018-12-02 23:01:43 +0900

    Twitter IDが設定されていない場合にエラーが発生するバグを修正

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtCoder Twitter Avatar
 // @namespace    https://ciffelia.com/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Display Twitter avatar on AtCoder user page
 // @author       prince <mc.prince.0203@gmail.com> (https://ciffelia.com/)
 // @license      MIT
@@ -17,19 +17,21 @@
 
   const twLinkElm = document.querySelector('a[href*="//twitter.com/"]');
 
-  const screenName = twLinkElm.innerText;
-  const avatarUrl = `https://avatars.io/twitter/${screenName}/small`;
+  if (twLinkElm !== null) {
+    const screenName = twLinkElm.innerText;
+    const avatarUrl = `https://avatars.io/twitter/${screenName}/small`;
 
-  const avatarElm = document.createElement('img');
-  avatarElm.src = avatarUrl;
-  avatarElm.referrerPolicy = 'no-referrer';
-  Object.assign(avatarElm.style, {
-    width: '20px',
-    height: '20px',
-    marginRight: '5px',
-    borderRadius: '50%'
-  });
+    const avatarElm = document.createElement('img');
+    avatarElm.src = avatarUrl;
+    avatarElm.referrerPolicy = 'no-referrer';
+    Object.assign(avatarElm.style, {
+      width: '20px',
+      height: '20px',
+      marginRight: '5px',
+      borderRadius: '50%'
+    });
 
-  twLinkElm.insertAdjacentElement('afterbegin', avatarElm);
+    twLinkElm.insertAdjacentElement('afterbegin', avatarElm);
+  }
 
 }());
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const twLinkElm = document.querySelector('a[href*="//twitter.com/"]')
 
-const screenName = twLinkElm.innerText
-const avatarUrl = `https://avatars.io/twitter/${screenName}/small`
+if (twLinkElm !== null) {
+  const screenName = twLinkElm.innerText
+  const avatarUrl = `https://avatars.io/twitter/${screenName}/small`
 
-const avatarElm = document.createElement('img')
-avatarElm.src = avatarUrl
-avatarElm.referrerPolicy = 'no-referrer'
-Object.assign(avatarElm.style, {
-  width: '20px',
-  height: '20px',
-  marginRight: '5px',
-  borderRadius: '50%'
-})
+  const avatarElm = document.createElement('img')
+  avatarElm.src = avatarUrl
+  avatarElm.referrerPolicy = 'no-referrer'
+  Object.assign(avatarElm.style, {
+    width: '20px',
+    height: '20px',
+    marginRight: '5px',
+    borderRadius: '50%'
+  })
 
-twLinkElm.insertAdjacentElement('afterbegin', avatarElm)
+  twLinkElm.insertAdjacentElement('afterbegin', avatarElm)
+}
